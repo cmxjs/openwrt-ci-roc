b6c39c4f3ba5df2b47c623423eb725a855cb056a
cmxjs <13226611660@163.com>
2026-04-07 14:51:05 +0800

    add istore

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,6 +1,6 @@
 # 修改默认IP & 固件名称 & 编译署名和时间
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='immortalWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.release\.description + ' / ' : '') + (luciversion || ''),# \
             _('Firmware Version'),\n \
             E('span', {}, [\n \
@@ -84,3 +84,5 @@ echo "baidu.com"  > package/luci-app-passwall/luci-app-passwall/root/usr/share/p
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
